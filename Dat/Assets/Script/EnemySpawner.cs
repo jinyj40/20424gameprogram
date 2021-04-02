4dbd9a2b29d7c685b6daa69a14ca7703c4eb1655
jinyj40 <jinyj40@gmail.com>
2021-04-02 17:16:49 +0900

    하하하하

---
+++ Dat/Assets/Script/EnemySpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Enemy enemy;
+    public GameObject enemyGroup;
+    private bool isPlaying = true;
+
+    public void OffSpawner(){
+        isPlaying = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            int spawnoer = Random.Range(0, 1001);
+            if (spawnoer < 100)
+            {
+                Enemy e = Instantiate(enemy);
+
+                float posY = Random.Range(-5f, 5f);
+                int isLeftInstatiate = Random.Range(0, 2);
+
+                if (isLeftInstatiate == 0)
+                {
+                    e.transform.position = new Vector3(-9f,posY, -1);
+                    e.transform.rotation = new Quaternion(0, 0,0, 0);
+                    e.SetDirectionVector(1);
+                }
+                else
+                {
+                    e.transform.position = new Vector3(9f, posY, -1);
+                    e.transform.rotation = new Quaternion(0, 0,180, 0);
+                    e.SetDirectionVector(-1);
+                }
+            }
+        }
+    }
+}
---
+++ Dat/Assets/Script/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int hp = 3;
+
+    public int GetHp()
+    {
+        return hp;
+    }
+    public void SetHP(int hp)
+    {
+        this.hp = hp;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp > 0)
+        {
+            float posx = Input.GetAxis("Horizontal");
+            float posy = Input.GetAxis("Vertical");
+
+            //float posx=0, posy=0;
+            //if (Input.GetKey(KeyCode.A))
+            //{
+            //    posx -=1;
+            //}
+            //if (Input.GetKey(KeyCode.D))
+            //{
+            //    posx += 1;
+            //}
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //    posy += 1;
+            //}
+            //if (Input.GetKey(KeyCode.S))
+            //{
+            //    posy -= 1;
+            //}
+            Vector3 pos = gameObject.transform.position;
+            pos.x += posx * Time.deltaTime * 10;
+            pos.y += posy * Time.deltaTime * 10;
+            if (pos.x > 7.9f)
+            {
+                pos.x = 7.9f;
+            }
+            else if (pos.x < -7.9)
+            {
+                pos.x = -7.9f;
+            }
+            if (pos.y > 4.47)
+            {
+                pos.y = 4.47f;
+            }
+            else if (pos.y < -4.47)
+            {
+                pos.y = -4.47f;
+            }
+            gameObject.transform.position = pos;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Enemy"))
+        {
+            hp--;
+            print(hp);
+        }
+    }
+}
